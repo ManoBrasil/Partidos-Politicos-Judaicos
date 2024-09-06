0d2296eb85b63064fd8499f84e805637c8924875
ManoBrasil <manobrasil.mb@gmail.com>
2024-09-06 11:56:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados = [
+    {
+      titulo: 'Saduceus',
+      descricao:  'Os saduceus, grupo composto por sacerdotes e membros da aristocracia judaica, eram conhecidos por suas visões conservadoras. Contrários à crença na ressurreição e em anjos, seus valores os colocavam em constante conflito com Jesus e seus discípulos. Com base em Jerusalém, os saduceus demonstravam desprezo pelas ideias de Jesus e perseguiam seus seguidores. Essa hostilidade é evidenciada em passagens bíblicas como Mateus 16:1-12 e Atos 4:1-4.',
+      link: 'https://pt.wikipedia.org/wiki/Saduceus',
+      tags: "perseguidores grupos opositores",      
+    },
+    {
+      titulo: 'Fariseus',
+      descricao: 'Os fariseus, uma seita judaica do Segundo Templo, eram conhecidos por sua rigorosa interpretação da Lei de Moisés, incluindo a tradição oral. Acreditavam na ressurreição, em anjos e em um juízo final, mas suas práticas muitas vezes eram vistas como hipócritas por Jesus (Mateus 23). Seus conflitos com Jesus, como as discussões sobre o sábado e a pureza ritual, surgiam da divergência na interpretação da Lei e da ênfase de Jesus no amor e na misericórdia. Apesar disso, alguns fariseus, como Nicodemos e José de Arimatéia, simpatizavam com Jesus em segredo.',
+      link: 'https://pt.wikipedia.org/wiki/Fariseus',
+      tags: "Mosaica hipocrisia farisaismo",      
+    },
+    {
+      titulo: 'Essênios',
+      descricao: 'Os essênios eram uma seita judaica do período do Segundo Templo, conhecida por seu estilo de vida ascético e comunitário. Isolados em comunidades no deserto, buscavam uma vida de pureza e devoção religiosa. Acreditavam em um mundo corrompido e aguardavam a chegada de um novo tempo, marcado pela intervenção divina. A purificação ritual era fundamental para os essênios, que praticavam batismos diários e seguiam uma dieta rigorosa. Viviam em comunidades comunais, onde todos os bens eram compartilhados e as decisões eram tomadas em conjunto. A interpretação da Torá era central para a vida dos essênios, e eles produziram os famosos Rolos do Mar Morto. Apesar de sua postura separatista em relação aos outros grupos judaicos, a comunidade essênia foi dizimada durante a Primeira Guerra Judaico-Romana. Os Rolos do Mar Morto, encontrados nas cavernas de Qumran, revelaram muitos detalhes sobre a vida e as crenças desse grupo enigmático.',
+      link: 'https://pt.wikipedia.org/wiki/Ess%C3%AAnios', 
+      tags: "religiosos comum",     
+    },
+    {
+      titulo: 'Zelotes',
+      descricao: 'Os zelotas eram um grupo radical de judeus que surgiu no século I d.C., durante o período do Segundo Templo. Conhecidos por seu fervor religioso e seu desejo de libertar o povo judeu do domínio romano, os zelotas empregavam táticas de guerrilha e assassinatos políticos para alcançar seus objetivos. Acreditando na pureza e na superioridade do povo judeu, eles buscavam estabelecer um Estado judeu independente. Apesar de sua devoção religiosa e interpretação estrita da Lei de Moisés, os zelotas eram vistos como extremistas por muitos de seus contemporâneos. Seu papel na Primeira Guerra Judaico-Romana foi crucial, mas a destruição de Jerusalém em 70 d.C. marcou o fim de sua luta armada. O legado dos zelotas é complexo, sendo vistos tanto como heróis da resistência quanto como terroristas, dependendo da perspectiva histórica.',
+      link: 'https://pt.wikipedia.org/wiki/Zelotes_de_Tessal%C3%B4nica',
+      tags: "radiacais mosaica",      
+    },
+    {
+      titulo: 'Herodianos',
+      descricao: 'Os herodianos eram uma facção judaica que surgiu durante o reinado de Herodes, o Grande. Essa seita, embora minoritária e heterogênea, era composta por indivíduos que, por diversos motivos, apoiavam a dinastia herodiana e a dominação romana. A lealdade dos herodianos à família de Herodes era motivada por diversos fatores, como interesses políticos, econômicos ou simplesmente a crença de que a estabilidade proporcionada pelo domínio romano era benéfica para o povo judeu. Essa facção era frequentemente vista com desdém pelos outros grupos judaicos, como os fariseus e os saduceus, que eram mais nacionalistas e religiosos. A influência dos herodianos diminuiu significativamente após a morte de Herodes, mas seus seguidores continuaram a existir por algum tempo, especialmente entre a elite sacerdotal e a aristocracia judaica. A história dos herodianos nos mostra como a fé e a política podem se entrelaçar de forma complexa, e como a lealdade a um líder pode transcender as divisões religiosas.',
+      link: 'https://pt.wikipedia.org/wiki/Herodianos',
+      tags: "apoiadores familiares"      
+    }
+  ];
+
